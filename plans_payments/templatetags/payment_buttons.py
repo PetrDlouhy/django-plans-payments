7c537d05394b789e6b9e991ddd2433bfd3574361
Petr Dlouhý <petr.dlouhy@email.cz>
2018-08-05 13:34:44 +0200

    clarify and simplify payment buttons code

--- plans_payments/templatetags/payment_buttons.py
+++ plans_payments/templatetags/payment_buttons.py
@@ -8,12 +8,8 @@ register = template.Library()
 
 # settings value
 @register.inclusion_tag("plans_payments/payment_buttons.html")
-def settings_value(object_variable):
+def payment_buttons(object_variable):
     variants = getattr(settings, 'PAYMENT_VARIANTS', [])
-    #for variant in variants:
-    #    variant.url = reverse("create_payment", kwargs={'payment_variant': 'default', 'order_id': object_id})
-
-    print(variants)
     return {
         'variants': variants,
         'object': object_variable,
